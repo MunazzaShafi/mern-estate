00acc805960aece4257233cda08532c38a1846ea
MunazzaShafi <munazzashafi120@gmail.com>
2026-08-31 13:44:16 +0500

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express =require("express");
+const mongoose = require("mongoose");
+const dotenv= require("dotenv");
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("connected to MongoDB")
+}).catch((err)=>{
+    console.log(err)
+});
+ const app= express();
+
+ app.listen(3000 ,()=>{
+    console.log("Server is running on port :3000")
+ })
